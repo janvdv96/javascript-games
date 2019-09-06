541840963f6b005063d00f3d2e80a9aaa3e3b36d
janvdv96 <janvandevelde.dev@gmail.com>
2019-09-06 10:40:54 +0200

    made blox page fancier

---
+++ blocks/blocksapp.js
@@ -0,0 +1,8 @@
+
+let canvas = document.getElementById("canvas");
+let ctx = canvas.getContext("2d");
+
+ctx.font = "78px Comic Sans MS";
+ctx.fillStyle = "white";
+ctx.textAlign = "center";
+ctx.fillText("Blox™", canvas.width/2, canvas.height/2 + 20);
